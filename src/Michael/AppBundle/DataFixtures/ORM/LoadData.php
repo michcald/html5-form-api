fc3f67cbc11afc6cbb7525d32c374248002a767f
Michael <michael@debian>
2013-12-08 12:18:04 +0000

    Adding empty fixture

---
+++ src/Michael/AppBundle/DataFixtures/ORM/LoadData.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Michael\AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        
+    }
+}
