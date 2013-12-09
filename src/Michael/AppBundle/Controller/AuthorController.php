1c435c6d4901825a9598326c692c756673e7462f
Michael <michael@debian>
2013-12-09 20:19:11 +0000

    Improvements

--- src/Michael/AppBundle/Controller/AuthorController.php
+++ src/Michael/AppBundle/Controller/AuthorController.php
@@ -9,6 +9,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 // required for REST
 use FOS\RestBundle\Controller\Annotations as Rest;
 
+// for documentation
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
@@ -26,6 +29,19 @@ class AuthorController extends Controller
 	protected $repository = 'MichaelAppBundle:Author';
 
 	/**
+     * This the documentation description of your method, it will appear
+     * on a specific pane. It will read all the text until the first
+     * annotation.
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="This is a description of your API method",
+     *  filters={
+     *      {"name"="a-filter", "dataType"="integer"},
+     *      {"name"="another-filter", "dataType"="string", "pattern"="(foo|bar) ASC|DESC"}
+     *  }
+     * )
+     *
      * @Route(
      *      "/authors",
      *      name = "app.route.author.all",
